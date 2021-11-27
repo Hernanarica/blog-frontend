e4760b4a47e64f80ddc1d906d01656fdbaecdb5d
Hernán <hernanaricammm@gmail.com>
2021-11-27 18:00:37 -0300

    Proyecto base listo, con estructura de carpetas y archivos

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+function NavBar() {
+	return (
+		 <header>
+			 <h1>
+				 <img src="" alt="En construcción"/>
+				 My Blog
+			 </h1>
+			 <nav>
+				 <ul>
+					 <li>
+						 <a href="#">Home</a>
+					 </li>
+					 <li>
+						 <a href="#">Login</a>
+					 </li>
+					 <li>
+						 <a href="#">Registro</a>
+					 </li>
+				 </ul>
+			 </nav>
+		 </header>
+	);
+}
+
+export default NavBar;
