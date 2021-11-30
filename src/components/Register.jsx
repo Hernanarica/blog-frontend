0d55f3a8a320b268e6afae29981a9bb2a0d21afe
Hernán Arica <hernanaricammm@gmail.com>
2021-11-29 23:47:55 -0300

    Registro de usuarios integrado

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,58 @@
-import '../css/components/register.css'
+import '../css/components/register.css';
+import { useState } from "react";
 
-function Register() {
-    return (
-        <section className="registro">
-            <h1 className="registro__h1">
-               Registro de usuarios
-            </h1>
-
-        </section>
-    );
+function Register(props) {
+	const [ name, setName ]         = useState("");
+	const [ lastname, setLastname ] = useState("");
+	const [ email, setEmail ]       = useState("");
+	const [ password, setPassword ] = useState("");
+	
+	function onLoginSubmit(e) {
+		e.preventDefault();
+		
+		
+		fetch('http://localhost/user/api-userCreate', {
+			method: "POST", headers: {
+				'Content-Type': 'application/json'
+			}, body: JSON.stringify({
+				name, lastname, email, password
+			})
+		})
+			 .then(function (res) {
+				 return res.json();
+			 })
+			 .then(function (data) {
+				 console.log(data);
+			 })
+			 .catch(function (err) {
+				 console.log(err);
+			 });
+	}
+	
+	return (<section className="registro">
+		<h2 className="registro__h1">
+			Registro de usuarios
+		</h2>
+		<form onSubmit={ (e) => onLoginSubmit(e) } action="" className="sectionLogin__form">
+			<div className="sectionLogin__labels">
+				<label htmlFor="">Nombre</label>
+				<input type="text" value={ name } onChange={ (e) => setName(e.target.value) }/>
+			</div>
+			<div className="sectionLogin__labels">
+				<label htmlFor="">Apellido</label>
+				<input type="text" value={ lastname } onChange={ (e) => setLastname(e.target.value) }/>
+			</div>
+			<div className="sectionLogin__labels">
+				<label htmlFor="">Email</label>
+				<input type="email" value={ email } onChange={ (e) => setEmail(e.target.value) }/>
+			</div>
+			<div className="sectionLogin__labels">
+				<label htmlFor="">Password</label>
+				<input type="password" value={ password } onChange={ (e) => setPassword(e.target.value) }/>
+			</div>
+			<button className="sectionLogin__btn">Registrar</button>
+		</form>
+	</section>);
 }
 
 export default Register;
