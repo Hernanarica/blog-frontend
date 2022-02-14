b01d52893080698b97b459630aa5fd4a27e4817d
sersebas08 <85594721+sebaservente@users.noreply.github.com>
2022-02-14 15:10:59 -0300

    cambio de enpoint register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,7 +13,7 @@ function Register(props) {
 		e.preventDefault();
 		
 		
-		fetch('http://localhost:9001/user/api-user', {
+		fetch('http://localhost:9001/api/user/create', {
 			method: "POST", headers: {
 				'Content-Type': 'application/json'
 			}, body: JSON.stringify({
